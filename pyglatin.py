cde1443ec8067f5866e0c74c5079b62634fcb1be
Bryan Hernandez <pdnbryan1998@gmail.com>
2017-10-30 12:12:39 -0700

    Update pyglatin.py

--- pyglatin.py
+++ pyglatin.py
@@ -2,7 +2,7 @@ def main():
 	words = input('Enter some words: ').split()
 	for word in words:
 		if word[0] in ['a','e','i','o','u']:
-			print(word[1:] + word[0] + "yay", end = " ")
+			print(word[1:] + word[0] + "way", end = " ")
 		else:
 			print(word[1:] + word[0] + "ay", end = " ")
 	print()
