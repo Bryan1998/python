67e1653cef83be8dd45cc14c3324350cca6c722e
Bryan1998 <pdnbryan1998@gmail.com>
2017-10-30 13:19:31 -0700

    added all

--- random_char_all.py
+++ random_char_all.py
@@ -1,9 +1,8 @@
-import random
+from random import shuffle
 def randlist():
 	sentence = "The quick brown fox jumps over the lazy dog".replace(" ", "")
 	misc = "01234567890 `[]\\;\',./~!@#$%^&*()_+{}|:\"<>?"
 	alphabet = sorted(set(sentence.lower() + sentence.upper() + misc))
-	random.shuffle(alphabet)
+	shuffle(alphabet)
 	print(''.join(alphabet))
-for i in range(128):
-	randlist()
+randlist()
