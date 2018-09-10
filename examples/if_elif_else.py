096aeb40beab67f9470514a49c053f93a4cdc8cf
Bryan Hernandez <pdnbryan1998@gmail.com>
2018-09-10 09:43:10 -0700

    Add files via upload

---
+++ examples/if_elif_else.py
@@ -0,0 +1,7 @@
+#/usr/bin/python
+
+word = raw_input("Enter a word:")
+word = word.lower()
+
+if word == "bird":
+	print("Everybody knows about the bird\nbird bird bird\nbird is the word.")
---
+++ examples/if_else.py
@@ -0,0 +1,9 @@
+#/usr/bin/python
+
+integer = raw_input("Enter an integer: ")
+integer = int(integer)
+
+if integer > 50:
+	print("Integer is greater than 50.")
+else:
+	print("Integer is less than 50.")
