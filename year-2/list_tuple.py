4d03c7433cb80afd75f8e16be20d3da25e575029
Bryan Hernandez <pdnbryan1998@gmail.com>
2018-11-13 12:58:13 -0800

    Added all...

--- year-2/list_tuple.py
+++ year-2/list_tuple.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #
 # list_tuple.py bryan
 import pprint
@@ -21,11 +21,11 @@ def ThreeMerge(a, b, c):
 	return lst1 + lst2
 
 def NatoTuple():
-	nato = {"A":"Alpha", "B":"Bravo", "C":"Charlie", "D":"Delta", "E":"Echo", "F":"Foxtrot", "G":"Golf", "H":"Hotel", "I":"India", "J":"Juliet", "K":"Kilo", "L":"Lima", "M":"Mike", "N":"November", "O":"Oscar", "P":"Papa", "Q":"Quebec", "R":"Romeo", "S":"Sierra", "T":"Tango", "U":"Uniform", "V":"Victor", "W":"Whiskey", "X":"Xray", "Y":"Yankee", "Z":"Zulu"}
+	nato = (["A", "Alpha"], ["B", "Bravo"], ["C", "Charlie"], ["D", "Delta"], ["E", "Echo"], ["F", "Foxtrot"], ["G", "Golf"], ["H", "Hotel"], ["I", "India"], ["J", "Juliet"], ["K", "Kilo"], ["L", "Lima"], ["M", "Mike"], ["N", "November"], ["O", "Oscar"], ["P", "Papa"], ["Q", "Quebec"], ["R", "Romeo"], ["S", "Sierra"], ["T", "Tango"], ["U", "Uniform"], ["V", "Victor"], ["W", "Whiskey"], ["X", "Xray"], ["Y", "Yankee"], ["Z", "Zulu"])
 	return nato
 
 print("1D List:")
-pp(One(1))
+pp(One(11))
 
 print("\n2D List:")
 pp(Two(2, 2))
